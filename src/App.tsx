15ee1952b5444dd08263de6019aac3213f793eb1
Ilaniya <evgeniya.pavlovich@gmail.com>
2022-04-28 11:57:09 +0300

    add json file and display it's datas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MyInfo from './aboutMe.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        MyInfo && MyInfo.map(info => {
+          return (
+            <div className='box' key={info.id}>
+              <strong>{info.name}</strong><br/>
+              <p>{info.biography}</p>
+              <p>{info.contacts}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
